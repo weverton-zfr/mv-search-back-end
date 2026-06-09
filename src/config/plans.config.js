578ef187a3552c4da02796a9566f7a8198485bfe
Weverton Ribeiro <wevertonribeiro009@gmail.com>
2026-06-09 17:51:19 -0300

    Fix: ALteração nos IDs dos planos

--- src/config/plans.config.js
+++ src/config/plans.config.js
@@ -1,15 +1,15 @@
 export const plans = {
-  "6a12868f7d9c8f18eb320995": {
+  "6a27363aec33f20ebcf057ca": {
     name: "Plano Basic Mensal",
     valueCents: 3999
   },
 
-  "6a1286af7d9c8f18eb320a9c": {
+  "6a273665ec33f20ebcf05de3": {
     name: "Plano Basic Trimensal",
     valueCents: 9999
   },
 
-  "6a1286bb7d9c8f18eb320b67": {
+  "6a273676ec33f20ebcf05fe9": {
     name: "Plano Anual",
     valueCents: 19999
   }
