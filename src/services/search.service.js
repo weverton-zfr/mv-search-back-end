f99c24d46822d0d2d17268a2967df29e0de96256
Weverton Ribeiro <wevertonribeiro009@gmail.com>
2026-06-13 23:46:58 -0300

    teste de agente api

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -6,23 +6,20 @@ export async function executeSearch(params) {
       ? process.env.SEARCH_API_NAME_URL
       : process.env.SEARCH_API_URL;
 
-  console.log("MODULO:", params.modulo);
-  console.log("BASE URL:", baseURL);
-  console.log("SEARCH_API_URL existe:", !!process.env.SEARCH_API_URL);
-  console.log("SEARCH_API_NAME_URL existe:", !!process.env.SEARCH_API_NAME_URL);
-  console.log("SEARCH_API_TOKEN existe:", !!process.env.SEARCH_API_TOKEN);
-
   const response = await axios.get(baseURL, {
     params: {
       token: process.env.SEARCH_API_TOKEN,
       ...params
+    },
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Accept": "application/json"
     },
     timeout: 30000,
     validateStatus: () => true
   });
 
   console.log("STATUS API EXTERNA:", response.status);
-  console.log("RESPOSTA API EXTERNA:", response.data);
 
   return response.data;
 }
