3c418761ad2504f34890af06ef23a4be7117f14f
Weverton Ribeiro <wevertonribeiro009@gmail.com>
2026-06-15 04:43:20 -0300

    get ip

--- src/routes/me.routes.js
+++ src/routes/me.routes.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import { getMe } from '../controllers/me.controller.js'
 import { authMiddleware } from '../middleware/auth.middleware.js'
+import axios from "axios";
 
 const router = express.Router()
 
 router.get('/me', authMiddleware, getMe)
+
+
+
+router.get("/my-ip", async (req, res) => {
+  try {
+    const { data } = await axios.get("https://api.ipify.org?format=json");
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar IP" });
+  }
+});
+
 
 export default router
