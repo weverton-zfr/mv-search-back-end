bd2472cbcd50194a285e247c6efde4b1ce5106d8
Weverton Ribeiro <wevertonribeiro009@gmail.com>
2026-06-15 04:47:10 -0300

    BACKUP

--- src/routes/me.routes.js
+++ src/routes/me.routes.js
@@ -7,16 +7,4 @@ const router = express.Router()
 
 router.get('/me', authMiddleware, getMe)
 
-
-
-router.get("/my-ip", async (req, res) => {
-  try {
-    const { data } = await axios.get("https://api.ipify.org?format=json");
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar IP" });
-  }
-});
-
-
 export default router
