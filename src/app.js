731e68103805aae51e549d54d268df3d89800dee
Weverton Ribeiro <wevertonribeiro009@gmail.com>
2026-06-17 21:19:09 -0300

    fix: app cors config

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
    origin: [
-      "http://localhost:5173",
+      "https://www.mvsearch.com.br/",
     ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
